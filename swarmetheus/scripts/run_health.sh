8317e3024a041cb5f249e04d7f8106f6460eb001
Ryan Jones <rjchicago@gmail.com>
2022-02-22 08:46:04 -0600

    instance rule and health refactor

--- swarmetheus/scripts/run_health.sh
+++ swarmetheus/scripts/run_health.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -eu
 
-NAME="health"
+CONTAINER_NAME="swarmetheus-health"
 IMAGE=$(docker inspect -f "{{.Config.Image}}" $(hostname))
-echo "STARTING: swarmetheus-health"
-docker container rm swarmetheus-health -f 2> /dev/null
+echo "STARTING: $CONTAINER_NAME"
+docker container rm $CONTAINER_NAME -f 2> /dev/null
 docker run \
   -d \
   --rm \
   --init \
   --pid=host \
   --label hidden \
-  --name="swarmetheus-$NAME" \
+  --name="$CONTAINER_NAME" \
   --entrypoint="/swarmetheus/scripts/health.sh" \
   -e PARENT_PID="$(docker inspect -f "{{.State.Pid}}" $(hostname))" \
   -e STARTUP_SECONDS=${STARTUP_SECONDS:-30} \
